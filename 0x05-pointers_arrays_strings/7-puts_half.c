78ed685fb084077492132498e5a85b44e104d5f0
oyis12 <oyivosam12@gmail.com>
2023-07-24 13:21:39 +0100

    fixed 7-puts_half.c and 6-puts2.c error

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,23 +1,25 @@
 #include "main.h"
-#include <stdio.h>
-#include <string.h>
-
 /**
- * puts_half - prints the second half of a string
- * @str: string to print
- *
- * Return: void
-*/
+ * puts_half - a function that prints half of a string
+ * if odd len, n = (length_of_the_string - 1) / 2
+ * @str: input
+ * Return: half of input
+ */
 void puts_half(char *str)
 {
-int length = strlen(str);
-int n = (length - 1) / 2;
-if (length % 2 == 0)
-{
-printf("%s\n", str + length / 2);
-}
-else
-{
-printf("%s\n", str + n);
-}
+	int a, n, longi;
+
+	longi = 0;
+
+	for (a = 0; str[a] != '\0'; a++)
+		longi++;
+
+	n = (longi / 2);
+
+	if ((longi % 2) == 1)
+		n = ((longi + 1) / 2);
+
+	for (a = n; str[a] != '\0'; a++)
+		_putchar(str[a]);
+	_putchar('\n');
 }
