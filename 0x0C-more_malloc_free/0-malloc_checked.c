037fe6a7dd5d0c202ded07bc1ecaf3f41e6629cb
oyis12 <oyivosam12@gmail.com>
2023-08-10 12:22:56 +0100

    first task done

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+
+/**
+* malloc_checked - cause normal process termination with a status value of 98
+*
+* @b: allocated memory
+*
+* Return: a pointer to the allocated memory
+*/
+void *malloc_checked(unsigned int b)
+{
+void *ptr;
+ptr = malloc(b);
+if (ptr == NULL)
+exit(98);
+return (ptr);
+}
---
+++ 0x0C-more_malloc_free/README.md
@@ -0,0 +1,1 @@
+This directory is for the 0x0C-more_malloc_free project task
