22065aad7eefb5c63ee655a41cc1ed5819b604da
oyis12 <oyivosam12@gmail.com>
2023-08-14 11:01:00 +0100

    first commit to this directory

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program.
+ *
+ * Return: Always 0 (Success)
+*/
+int main(void)
+{
+	printf("Compiled from file: %s\n", __FILE__);
+	return (0);
+}
---
+++ 0x0D-preprocessor/README.md
@@ -0,0 +1,1 @@
+s directory is for the 0x0D-preprocessor project task
