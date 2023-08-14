49a810bd5f9b3f7b51dd7fd0435b7fae1b0632ee
oyis12 <oyivosam12@gmail.com>
2023-08-14 13:42:29 +0100

    fix task 2

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
 /**
- * main - Entry point of the program.
+ * main - prints the name of the file
  *
  * Return: Always 0 (Success)
-*/
+ */
 int main(void)
 {
-	printf("Compiled from file: %s\n", __FILE__);
+	printf("%s\n", __FILE__);
 	return (0);
 }
