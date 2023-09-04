4652e3bf10553d41a2e29ffc50b7de1ee170f5e8
oyis12 <oyivosam12@gmail.com>
2023-09-04 16:55:50 +0100

    task 01 and 1 done

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * read_textfile - Read text file and print to STDOUT.
+ * @filename: Text file being read.
+ * @letters: Number of letters to be read.
+ *
+ * Return: Actual number of bytes read and printed, 0 when function fails or filename is NULL.
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	char *buf;
+	ssize_t fd;
+	ssize_t w;
+	ssize_t t;
+
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
+
+	buf = malloc(sizeof(char) * letters);
+	if (buf == NULL)
+	{
+		close(fd);
+		return (0);
+	}
+
+	t = read(fd, buf, letters);
+	w = write(STDOUT_FILENO, buf, t);
+
+	free(buf);
+	close(fd);
+
+	return (w);
+}
---
+++ 0x15-file_io/README.md
@@ -0,0 +1,1 @@
+This directory is for 0x15-file_io project task
